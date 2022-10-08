bd1101fd04bf7f3456a333dd3693404a7b442685
NahLima <naiarafx@gmail.com>
2022-10-08 19:16:34 -0300

    remove senha

--- config_boletim/settings.py
+++ config_boletim/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qvo$893c&bte7@$xn$nr4bsx*la@3v#27s$)405-3#47_#@-&q'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
